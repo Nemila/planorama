7bdc5ba311277190831b3eed6c9d510cecba57de
Lamine <pridila.2006@gmail.com>
2023-04-29 23:38:25 +0000

    edit event card and add momentjs

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -4,9 +4,18 @@ import Link from "next/link";
 import { toast } from "react-toastify";
 import { useSWRConfig } from "swr";
 import DeleteModal from "./DeleteModal";
+import moment from "moment/moment";
+import { HiCalendar, HiChevronDown, HiMapPin, HiTrash } from "react-icons/hi2";
 
 const EventCard = ({ event }) => {
   const { mutate } = useSWRConfig();
+
+  const startDate = moment(event.startAt);
+  const updatedAt = [
+    moment(event.updatedAt).year(),
+    moment(event.updatedAt).month(),
+    moment(event.updatedAt).date(),
+  ];
 
   const handleEventDelete = async () => {
     try {
@@ -24,7 +33,7 @@ const EventCard = ({ event }) => {
         id={event.id}
         className="card compact min-w-[300px] max-w-md flex-1 bg-base-100 shadow-md"
       >
-        <figure className="h-32 w-full overflow-hidden">
+        <figure className="h-32 overflow-hidden">
           <Image
             className="h-full w-full object-cover"
             alt="banner"
@@ -33,23 +42,38 @@ const EventCard = ({ event }) => {
             src={`https://vwpobyxervyuezweoaju.supabase.co/storage/v1/object/public/events/${event?.image}`}
           />
         </figure>
+
         <div className="card-body">
-          <div
-            className={`badge ${
-              event.status === "ONGOING" && "badge-primary"
-            } ${event.status === "FINISHED" && "badge-secondary"}`}
-          >
-            {event.status}
-          </div>
           <h2 className="card-title line-clamp-1">{event.name}</h2>
-          <p className="line-clamp-2">{event.description}</p>
+
+          <div className="flex gap-2">
+            <div className="flex items-center gap-1">
+              <HiCalendar className="text-lg" />
+              <p className="text-xs">{startDate.format("LL")}</p>
+            </div>
+
+            <div className="flex items-center gap-1">
+              <HiMapPin className="text-lg" />
+              <p className="text-xs">{event.location}</p>
+            </div>
+          </div>
+
+          <p className="my-2 line-clamp-2">{event.description}</p>
+
+          <p className="text-xs">Last update {moment(updatedAt).fromNow()}</p>
+
           <div className="card-actions justify-end">
-            <Link className="btn-primary btn" href={`/events/${event.id}`}>
-              Details
-            </Link>
-            <label htmlFor={`deleteEventModal-${event.id}`} className="btn">
-              Delete
-            </label>
+            <div className="btn-group">
+              <Link className="btn" href={`/events/${event.id}`}>
+                Edit
+              </Link>
+              <label
+                htmlFor={`deleteEventModal-${event.id}`}
+                className="btn-outline btn-square btn"
+              >
+                <HiTrash className="text-xl" />
+              </label>
+            </div>
           </div>
         </div>
       </div>
